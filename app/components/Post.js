89203d19ee6c30ee952383250edd375c9c87d41f
Sumit Saha <sumitsaha1233@gmail.com>
2024-02-22 21:05:24 +0530

    Read More and Read Less functionality added in Post Component.

--- app/components/Post.js
+++ app/components/Post.js
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,21 +7,42 @@ import {
     faComment,
     faShareFromSquare
 } from "@fortawesome/free-solid-svg-icons";
+
+let postDataStyle = {
+
+    overflow: "hidden",
+    display: "-webkit-box",
+    WebkitBoxOrient: "vertical",
+    WebkitLineClamp: 3
+}
+
 const Post = () => {
+    const [isOpen, setIsOpen] = React.useState(false)
+    const [isShowReadMoreButton, setIsShowReadMoreButton] = React.useState(false)
+    const ref = React.useRef(null)
+
+    React.useEffect(() => {
+        if (ref.current) {
+            setIsShowReadMoreButton(ref.current.scrollHeight !== ref.current.clientHeight)
+        }
+    }, [])
     return (
-        <div className='border-2 border-solid border-dark w-72 m-auto mt-2 flex flex-col justify-center '>
+        <div className='border-2 border-solid border-dark w-72 sm:w-[440px] m-auto mt-2 flex flex-col justify-center '>
             <div className='flex justify-start gap-4 p-1 items-center border-b-2 w-full'>
                 <Avatar>
                     <AvatarImage src="https://github.com/shadcn.png" />
                     <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
-                <div className='name text-sm'>Sumit Saha</div>
+                <div className='name text-md'>Sumit Saha</div>
             </div>
-            <div className='postcaption text-[12px] pl-1'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod natus maxime quo possimus. Provident ex maxime suscipit fuga voluptas sint! Facilis adipisci inventore odio? Veritatis omnis exercitationem perspiciatis natus non animi dolor!
+            <div className='postcaption text-md pl-1' style={isOpen ? null : postDataStyle} ref={ref}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda aperiam consequatur asperiores at voluptatibus, exercitationem dolores culpa corrupti, molestias dolorem, ea iste sed inventore numquam eos in enim odit. Ea impedit repellat recusandae fugit quae cum nisi mollitia, a veniam molestiae neque ratione iste ipsam maxime voluptatibus perspiciatis earum consequatur? Vero hic corrupti ullam dolorem molestias quae, minima magnam quod perspiciatis reiciendis similique in? Adipisci temporibus, quibusdam id incidunt placeat ullam? Placeat vero alias quaerat architecto iusto magni.
             </div>
+            {isShowReadMoreButton ?
+                <button className='self-end' onClick={() => setIsOpen(!isOpen)}>{isOpen ? "Read Less" : "Read More"}</button> : null
+            }
             <div className='postimage mt-1'>
-                <img src="https://github.com/shadcn.png" className='pl-4 pr-4 w-full h-48' />
+                <img src="https://github.com/shadcn.png" className='pl-4 pr-4 w-full h-70' />
             </div>
             <div className='postimpression w-full flex justify-evenly items-center mt-2 p-1'>
                 <FontAwesomeIcon className='text-2xl' icon={faHeart} />
