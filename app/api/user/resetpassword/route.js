bae3f80d37d06939a5818a457262b801ed132314
Sumit Saha <sumitsaha1233@gmail.com>
2024-03-12 20:14:46 +0530

    Forget Password Functionality done.

--- app/api/user/resetpassword/route.js
+++ app/api/user/resetpassword/route.js
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import User from '../../../models/user'
 import jwt from "jsonwebtoken";
-import { redirect } from "next/navigation";
+import bcrypt from "bcryptjs"
+
 export async function GET(request) {
     let success = false;
     try {
@@ -13,8 +14,35 @@ export async function GET(request) {
             return NextResponse.json({ success, errorMessage: "User Not Exist" }, { status: 404 })
         }
         let secret = process.env.JWT_SECRET_KEY + user.password;
-        jwt.verify(token, secret)
+        let data = jwt.verify(token, secret)
         success = true;
+        return NextResponse.redirect(new URL(`/resetpasswordopino?id=${id}&token=${token}`, request.url))
+    } catch (error) {
+        return NextResponse.json({ success, errorMessage: "Internal Server Error" }, { status: 400 })
+    }
+
+}
+
+export async function POST(request) {
+    let success = false;
+    try {
+        let searchParams = request.nextUrl.searchParams
+        let id = searchParams.get("id")
+        let token = searchParams.get("token")
+        let { password } = await request.json()
+        let user = await User.findById({ _id: id })
+        if (!user) {
+            return NextResponse.json({ success, errorMessage: "User Not Exist" }, { status: 404 })
+        }
+        let secret = process.env.JWT_SECRET_KEY + user.password;
+        jwt.verify(token, secret)
+
+        //Creating secure password using salt and hash
+        let salt = await bcrypt.genSalt(10)
+        let securePassword = bcrypt.hashSync(password, salt)
+        await User.findByIdAndUpdate(id, { password: securePassword }, { new: true })
+        success = true;
+
         return NextResponse.json({ success })
     } catch (error) {
         return NextResponse.json({ success, errorMessage: "Internal Server Error" }, { status: 400 })
--- app/resetpasswordopino/page.js
+++ app/resetpasswordopino/page.js
@@ -8,63 +8,68 @@ import { Button } from "@/components/ui/button"
 import {
     Card,
     CardContent,
-    CardDescription,
     CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+
 
 const page = () => {
-
-    let [email, setEmail] = useState("");
+    let params = useSearchParams()
+    let id = params.get("id")
+    let token = params.get("token")
     let router = useRouter()
-    const handleForgetPassword = async () => {
+    const [password, setPassword] = useState("")
+
+    const handleResetPassword = async () => {
         try {
-            await fetch("http://localhost:3000/api")
-            let res = await fetch("http://localhost:3000/api/user/forgetpassword", {
+            await fetch('http://localhost:3000/api')
+            let res = await fetch(`http://localhost:3000/api/user/resetpassword?id=${id}&token=${token}`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({ email: email })
+                body: JSON.stringify({ password })
             })
-            let json = await res.json()
-            if (json.success) {
-                console.log(json.link)
+            let data = await res.json()
+            if (data.success) {
+                router.push("/login")
             }
             else {
-                console.log(json.errorMessage)
+                alert("Some Error Occured")
             }
         } catch (error) {
-            console.log("Some internal error")
+            console.log("Something went wrong")
         }
     }
-
     return (
         <div>
             <Card className="w-[350px] m-auto mt-6">
                 <FontAwesomeIcon icon={faCircleUser} className="m-auto w-full text-5xl -mt-8 z-10 text-orange-500" />
                 <CardHeader>
-                    <CardTitle>Forget Password</CardTitle>
+                    <CardTitle>Reset Password </CardTitle>
 
                 </CardHeader>
                 <CardContent>
                     <form>
                         <div className="grid w-full items-center gap-4">
                             <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="email">Email</Label>
-                                <Input id="email" type="email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                <Label htmlFor="email">Password</Label>
+                                <Input id="password" type="password" placeholder="New Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                             </div>
-
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="email">Confirm Password</Label>
+                                <Input id="cpassword" type="password" placeholder="Confirm Password" />
+                            </div>
                         </div>
                     </form>
                 </CardContent>
 
                 <CardFooter className="flex justify-between">
-                    <Button className="bg-orange-400" onClick={handleForgetPassword}>Send Code</Button>
+                    <Button className="bg-orange-400" onClick={handleResetPassword}>Submit</Button>
                 </CardFooter>
             </Card>
         </div>
