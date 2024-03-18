275461c1519e3fa5ad1ffaf8a92d55a59de14024
Sumit Saha <sumitsaha1233@gmail.com>
2024-03-18 20:44:35 +0530

    Mail text updated.

--- app/api/user/forgetpassword/route.js
+++ app/api/user/forgetpassword/route.js
@@ -11,7 +11,7 @@ export async function POST(request) {
             return NextResponse.json({ success, errorMessage: "User Not Found" }, { status: 404 })
         }
         const secret = process.env.JWT_SECRET_KEY + user.password;
-        const token = jwt.sign({ email: user.email, id: user._id }, secret, { expiresIn: "5m" })
+        const token = jwt.sign({ email: user.email, id: user._id }, secret, { expiresIn: "1d" })
         let link = `http://localhost:3000/api/user/resetpassword?id=${user._id}&token=${token}`
 
         var transporter = nodemailer.createTransport({
@@ -26,7 +26,13 @@ export async function POST(request) {
             from: 'opino90ac@gmail.com',
             to: email,
             subject: 'Password Reset',
-            text: link
+            text: `Hi,
+
+There was a request to change your password!
+
+If you did not make this request then please ignore this email.
+
+Otherwise, please click this link to change your password: ${link}`
         };
 
         transporter.sendMail(mailOptions, function (error, info) {
