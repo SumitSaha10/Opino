a164fea4852b630167891741b5804e7087577fca
Sumit Saha <sumitsaha1233@gmail.com>
2024-03-21 19:39:59 +0530

    Username field added in post model.

--- app/models/post.js
+++ app/models/post.js
@@ -9,6 +9,11 @@ const postSchema = new Schema({
         type: String,
         required: true
     },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
     postData: {
         type: String,
         required: true
